fb1c9c4cbb35245f804b98dc4289195f4838f6fb
Mariya Barska <moiweer@gmail.com>
2022-05-26 23:49:33 +0300

    1

---
+++ js/start.js
@@ -0,0 +1,18 @@
+
+// const base = 2;
+// const power = 5;
+// const result = Math.pow(base, power);
+// console.log(result);
+// const y = 6;
+// const x = 4;
+// console.log(x + y);
+// const x = 50;
+// const y = 30;
+// const summa = x + y
+// console.log(summa);
+// const a = 6;
+// const b = 10;
+// const pacificOcean;
+// console.log(pacificOcean = a);
+let username;
+console.log(username);
