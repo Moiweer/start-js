cf86a309e93eb81ae5cecc0815d2c8f192d06327
Mariya Barska <moiweer@gmail.com>
2022-05-27 02:06:21 +0300

    funcion

--- js/start.js
+++ js/start.js
@@ -14,5 +14,25 @@
 // const b = 10;
 // const pacificOcean;
 // console.log(pacificOcean = a);
-let username;
-console.log(username);
+// let username;
+// console.log(username);
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+
+function makeMessage(name, price) {
+    let message = (`You picked ${name}, price per item is ${price} credits`);
+
+    return message;
+    
+}
+console.log (makeMessage('Radar', 6150));
+console.log(makeMessage('Scanner', 3500));
+console.log(makeMessage('Reactor', 8000));
+console.log(makeMessage('Engine', 4070));
