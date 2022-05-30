8e1720977e44349f5fb65dae5e7b24edce7590ea
Mariya Barska <moiweer@gmail.com>
2022-05-30 16:02:22 +0300

    avtoproverka modul 1

--- js/secondpart.js
+++ js/secondpart.js
@@ -1,13 +1,18 @@
-function formatMessage(message, maxLength) {
-  let result;
+// function formatMessage(message, maxLength) {
+//   let result;
  
-    result = message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
+//     result = message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
 
-  return result;
+//   return result;
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16));
+// console.log(formatMessage("Curabitur ligula sapien", 23));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+
+const clients = ["Mango", "Ajax", "Poly"];
+
+for (let i = 0; i < clients.length; i += 1) {
+  console.log(clients[i]);
 }
-console.log(formatMessage("Curabitur ligula sapien", 16));
-console.log(formatMessage("Curabitur ligula sapien", 23));
-console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-
