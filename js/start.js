a85952ffbf614509943a38407eded42adec4ed7c
Mariya Barska <moiweer@gmail.com>
2022-12-02 21:07:27 +0200

    Update start.js

--- js/start.js
+++ js/start.js
@@ -1,3 +1,106 @@
+// 1 Change code below this line
+// const productName = "Droid";
+// const pricePerItem = 2000;
+// console.log(productName);
+// 'Droid'
+
+// console.log(pricePerItem);
+// 2000
+
+// 2
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// Change code below this line
+// productName = "Repair droid";
+// pricePerItem = 2000 + 1500;
+
+// 3
+// Change code below this line
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+// 4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// 5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+
+
+
+
+
+// 6
+
+// Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity + deliveryFee);
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+
+// 7
+
+// Change code below this line
+// function sayHi(){
+//  console.log("Hello, this is my first function!")
+// }
+// sayHi();
+
+// 8
+// Change code below this line
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+
+// 9
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+// return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+// 10
+
+// function makeMessage(name, price) {
+//     let message = (`You picked ${name}, price per item is ${price} credits`);
+
+//     return message;
+    
+// }
+// console.log (makeMessage('Radar', 6150));
+// console.log(makeMessage('Scanner', 3500));
+// console.log(makeMessage('Reactor', 8000));
+// console.log(makeMessage('Engine', 4070));
+
+
 
 // const base = 2;
 // const power = 5;
